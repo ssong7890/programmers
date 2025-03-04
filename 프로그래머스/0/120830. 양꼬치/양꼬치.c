4b449ec0158358684dd1ffcddca68538e5ed9d4e
Song <ssong7890@gmail.com>
2025-03-04 20:57:32 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.48 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int k) {
+    int answer = 0;
+    if (10 <= n);
+        answer = (12000 * n) + (2000 *k) - (n/10 * 2000);
+    return answer;
+}
